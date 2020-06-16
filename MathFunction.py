a56a63eedcbbd30a2551522e536f1e977a03e4b9
maulanawesome5 <60890352+Maulanawesome5@users.noreply.github.com>
2020-06-17 03:32:10 +0700

    This file contains source code from w3schools.com
    This python file explain how to learn Math modules

---
+++ MathFunction.py
@@ -0,0 +1,44 @@
+import math
+
+# Module math bisa digunakan pada PyCharm dan CMD
+
+# The min() and max() function can be used to find the
+# lowest or highest value in an iterable
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x)
+print(y)
+
+# the abs() function returns the absolute (Positive) value
+# of the specified number
+x = abs(-7.25)
+
+print(x)
+
+# The pow(x, y) function returns the value of x to the power
+# of y (x pangkat y)
+# Return the value of 4 to the power of 3 (same as 4*4*4)
+x = pow(4, 3)
+
+print(x)
+
+# The math.sqrt() method for example, return the square root of a number
+x = math.sqrt(64)
+
+print(x)
+
+# The math.ceil() method rounds a number upwards to its
+# nearest integer, and the math.floor() method rounds a number
+# downwards to its nearest integer, and return the result
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# The math.pi constant, return the value of PI (3,14)
+x = math.pi
+
+print(x)
+
