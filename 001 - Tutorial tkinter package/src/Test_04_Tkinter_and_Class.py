0f059717691701f3ae02faf8fa6025ffbdd2a37b
Maulanawesome5 <maulanawesome5@gmail.com>
2021-12-25 19:00:36 +0700

    Mengubah test04, menambahkan beberapa baris comment

--- 001 - Tutorial tkinter package/src/Test_04_Tkinter_and_Class.py
+++ 001 - Tutorial tkinter package/src/Test_04_Tkinter_and_Class.py
@@ -8,13 +8,16 @@ class FeetToMeters:
 
     def __init__(self, root):
 
+        # Memberikan judul pada jendela aplikasi
         root.title("Feet to Meters")
 
+        # Membuat isi untuk bagian content frame
         mainframe = ttk.Frame(root, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
        
+        # Membuat widget untuk entry / input dari user
         self.feet = StringVar()
         feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
         feet_entry.grid(column=2, row=1, sticky=(W, E))
@@ -27,18 +30,21 @@ class FeetToMeters:
         ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
         ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
+        # Adding some polish
         for child in mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
 
         feet_entry.focus()
         root.bind("<Return>", self.calculate)
         
+    # Membuat fungsi / method untuk melakukan kalkulasi feet to meters
     def calculate(self, *args):
         try:
             value = float(self.feet.get())
             self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
         except ValueError:
             pass
+
 
 root = Tk()
 FeetToMeters(root)
