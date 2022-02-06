a4b8c91e18fc763206a19eab090056f66a8e9155
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-06 14:44:20 +0700

    Latihan percabangan if else

---
+++ 18 - If Else statement/src/main.py
@@ -0,0 +1,25 @@
+# percabangan if else
+
+"""---------------
+    1. if
+    2. kondisi
+    3. aksi
+---------------"""
+
+nama = input('Siapa nama kamu? : ')
+
+# # percabangan if jenis inline (satu baris)
+"""if nama == 'aji': print(f"Halo {nama}") # inline if
+
+# percabangan if jenis multiline
+if nama == 'maulana':
+    print(f"Halo {nama}")
+    print(f"Hari ini cerah sekali.") """
+
+# percabangan if else
+if nama == 'aji':
+    print(f"Halo {nama}")
+    print(f"Hari ini cerah sekali")
+else:
+    print(f"Halo {nama}, salam kenal.")
+
