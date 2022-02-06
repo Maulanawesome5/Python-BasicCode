b34eb754d831228065c206e773a973bc35b91bd0
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-06 15:01:32 +0700

    Latihan ef elif else statement

---
+++ 19 - If Elif Else statement/src/main.py
@@ -0,0 +1,15 @@
+# percabangan if, elif, else
+
+nama = input('Siapa nama kamu? : ')
+
+# Program percabangan
+if nama == 'maulana':
+    # maulana sudah dikenali
+    print(f"Hai {nama}, hari yang cerah")
+elif nama == 'aji':
+    # aji sudah dikenali
+    print(f"Hai {nama}, jangan lupa belajar")
+else:
+    # belum dikenali, maka akan diucapkan salam kenal
+    print(f"Salam kenal {nama}")
+
