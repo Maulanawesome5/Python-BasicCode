bc45d2a63caecae5790586c8363f2f9f08bc9a5f
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-17 20:44:06 +0700

    Latihan while loop

---
+++ 22 - While Loop/src/main.py
@@ -0,0 +1,19 @@
+# Perulangan menggunakan while
+"""
+while kondisi:
+    aksi
+    syarat
+# akhir while
+"""
+
+angka = 0
+
+# while angka > 5:
+#     print(angka) 
+#     # akan loop terus karena kondisi selalu true
+
+while angka < 5:
+    angka += 1
+    print(angka)
+
+
