e75f3c7a9fcfb4e19ce4e7d8e4c16c73856407df
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-27 02:08:33 +0700

    KelasTerbuka : Fungsi dasar python

---
+++ 41 - Pengenalan Fungsi/src/main.py
@@ -0,0 +1,9 @@
+# Function / Method / Subroutine
+
+def hello_world():
+    print("Hello World!")
+    print("Kepada Ucup Surucup")
+    print("dan juga Otong Surotong")
+
+hello_world()
+hello_world()
